8e570ed3f3c5e7115e21cd1ed5d43eeb0252f1b8
mubrik <mubarakg4u@gmail.com>
2023-04-08 17:02:36 +0100

    Commited from Script

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,48 +1,41 @@
-# does stuff
-exec { 'apt_update':
-  command => '/usr/bin/apt-get update',
-  refreshonly => true,
-}
-
 package { 'nginx':
-  ensure => installed,
-  require => Exec['apt_update'],
+  ensure   => installed,
+  provider => 'apt'
 }
 
-file { '/var/www':
+file { '/var/www/html/':
   ensure => 'directory',
-  owner  => $::id,
-  group  => $::id,
-  mode   => '0755',
-}
-
-file { '/var/www/html':
-  ensure => 'directory',
-  owner  => $::id,
-  group  => $::id,
-  mode   => '0755',
-  require => File['/var/www'],
-}
-
-exec { 'change_owner':
-  command => "/bin/chown -R ${::id} /etc/nginx /var/www/html",
-  require => Package['nginx'],
+  before => Service['nginx']
 }
 
 file { '/var/www/html/index.html':
-  ensure => present,
-  content => "Hello World!\n",
-  require => Exec['change_owner'],
+  ensure  => 'file',
+  content => 'Hello World!',
+  require => File['/var/www/html/']
 }
 
-exec { 'add_header':
-  command => "/bin/grep -q 'add_header X-Served-By' /etc/nginx/nginx.conf || sudo /bin/sed -i '/# server_tokens off;/ a        add_header X-Served-By $hostname;' /etc/nginx/nginx.conf",
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => 'file',
+  content => 'server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+        add_header X-Served-By $hostname;
+        location /redirect_me {
+                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+        location / {
+                try_files $uri $uri/ =404;
+        }
+  }',
   require => Package['nginx'],
-  notify  => Service['nginx'],
+  before  => Service['nginx']
+
 }
 
 service { 'nginx':
-  ensure => running,
-  enable => true,
-  require => Exec['add_header'],
+  ensure  => 'running',
+  require => Package['nginx']
 }
