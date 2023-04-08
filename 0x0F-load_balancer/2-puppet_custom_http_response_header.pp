064472c9224370ce3be4d28eaf0011382f21bfc5
mubrik <mubarakg4u@gmail.com>
2023-04-08 17:04:03 +0100

    Commited from Script

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -18,18 +18,18 @@ file { '/var/www/html/index.html':
 file { '/etc/nginx/sites-enabled/default':
   ensure  => 'file',
   content => 'server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-        root /var/www/html;
-        index index.html index.htm index.nginx-debian.html;
-        server_name _;
-        add_header X-Served-By $hostname;
-        location /redirect_me {
-                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-        }
-        location / {
-                try_files $uri $uri/ =404;
-        }
+  listen 80 default_server;
+  listen [::]:80 default_server;
+  root /var/www/html;
+  index index.html index.htm index.nginx-debian.html;
+  server_name _;
+  add_header X-Served-By $hostname;
+  location /redirect_me {
+    return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+  }
+  location / {
+    try_files $uri $uri/ =404;
+  }
   }',
   require => Package['nginx'],
   before  => Service['nginx']
